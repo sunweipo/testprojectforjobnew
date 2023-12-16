8f591d0132222ca0f971a7777dcc14ee05f95274
12493 <1253086761@qq.com>
2023-12-16 14:56:05 +0800

    update 20231216 test 3

--- src/main/java/com/sun/testprojectforjob/controller/AisController.java
+++ src/main/java/com/sun/testprojectforjob/controller/AisController.java
@@ -25,6 +25,7 @@ public class AisController {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
+        System.out.println(4);
         return "ais_login";
     }
     @RequestMapping("/angular")
